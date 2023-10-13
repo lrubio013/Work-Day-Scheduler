b6ec7799e4908bd221d4c3e0fda9746ab2e096dc
lrubio013 <142952845+lrubio013@users.noreply.github.com>
2023-10-13 16:31:02 -0500

    Added my timeblock function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,33 @@
-var today= dayjs();
-var saveBTN= $(".saveBtn");
-var timeBlock= $(".time-block");
-
-
-
+var today = dayjs();
+var saveBTN = $(".saveBtn");
+var timeBlock = $(".time-block");
+var currentHour = dayjs().format('HH');
 
 // adds the date to the header
 $(function () {
   $("#currentDay").text(today.format("dddd, MMM D YYYY"));
 });
 
-$(function () {
 
-});
+// loops over time blocks
+timeBlock.each(function () {
+ // To check the time and add the classes for background indicators
+  if (this.id < currentHour) {
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+    $(this).addClass("past");
+  }
+  else if (this.id === currentHour) {
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+    $(this).addClass("present");
+  }
+  else {
+    $(this).removeClass("present");
+    $(this).removeClass("past");
+    $(this).addClass("future");
+
+  }
+})
+
+
